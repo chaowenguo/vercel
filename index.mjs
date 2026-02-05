a5c889ccdde287e458a50000d007a5519d2a651c
chaowenguo <chaowen.guo1@gmail.com>
2026-02-04 21:54:19 -0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,14 +1,14 @@
-import path from 'path'
-import {promises as fs} from 'fs'
 import Koa from 'koa'
-import serve from 'koa-static'
-import list from 'koa2-serve-index'
-import process from 'process'
-
+import { Router } from '@koa/router'
+ 
 const app = new Koa()
-app.use(list(import.meta.dirname)).use(serve(import.meta.dirname))
-//child_process.spawn('dotnet', [path.join(import.meta.dirname, 'Cli.dll'), 'start', 'accept', '--token', 'ELGPy/DEQYDtARslA6HnkrbPIF6JQi+qYLCre5LBe58='])
-//child_process.spawn(path.join(import.meta.dirname, 'bitpingd'))
-process.argv = [process.execPath, 'script.js', '--homeIp', 'point-of-presence.sock.sh', '--homePort', '443','--id', 'vercelcom0', '--version', '54', '--clientKey', 'proxyrack-pop-client', '--clientType', 'PoP']
-import('./script.js')
+const router = new Router()
+ 
+router.get('/', (ctx) => {
+  ctx.body = { message: 'Hello from Koa!' }
+})
+ 
+app.use(router.routes())
+app.use(router.allowedMethods())
+ 
 app.listen(3000)
